5f9689c42e2694c9672717752056397e88e80493
Vinícius Marques <viniciusboz1000@outlook.com>
2023-11-28 11:00:44 -0300

    Configurations almost conclude

---
+++ src/containers/index.jsx
@@ -0,0 +1,8 @@
+export { default as Blog } from './blog/Blog'
+export { default as Features } from './features/Features'
+export { default as Footer } from './footer/Footer'
+export { default as Header } from './header/Header'
+export { default as Possibility } from './possibility/Possibility'
+export { default as whatGPT } from './whatGPT3/WhatGPT3'
+
+// Verificar possibility com p minusculo e header com h minúsculo
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './possibility.css'
+
+const Possibility = () => {
+    return (
+        <div>
+            <h1>Possibiliy</h1>
+        </div>
+    )
+}
+
+export default Possibility;
